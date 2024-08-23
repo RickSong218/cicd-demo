288ea35b2b239c1ead21a0b829f1f4f2c165a514
unknown <285007604@qq.com>
2024-08-23 18:38:49 +0800

    Initial commit

---
+++ src/main/java/sg/edu/nus/iss/cicd_demo/CicdDemoApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.cicd_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/cicd_demo/CicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.cicd_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
